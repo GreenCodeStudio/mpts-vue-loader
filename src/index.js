56f7c6d7ac9835d787bf5812f6b5aa273ed27a72
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-12-04 00:35:31 +0100

    bugfix

--- src/index.js
+++ src/index.js
@@ -4,20 +4,21 @@ const TDocumentFragment = require("mpts-core/dist/nodes/TDocumentFragment.js").T
 module.exports = function loader(input) {
     let parsed = XMLParser.Parse(input)
     const script = parsed.children.find(x => x?.tagName?.toLocaleLowerCase() == 'script');
+    let scriptContent = 'export default {}';
     if (script) {
         let parsed2 = new TDocumentFragment()
         parsed2.children = parsed.children.filter(x => x !== script);
-        let scriptContent = script.children.map(x => x.text).join('\n');
-        return `
+        parsed = parsed2;
+        scriptContent = script.children.map(x => x.text).join('\n');
+    }
+    parsed.children = parsed.children.filter(x => x.text && x.text.trim() == '');
+    if (parsed.children.length == 1) {
+        parsed = parsed.children[0];
+    }
+
+    return `
     import { ref, h } from 'vue'
     import scriptCode from 'data:application/javascript;base64,${btoa(scriptContent)}'
-export default {render:function(variables){return ${parsed2.compileJSVue()}}, ...scriptCode}
+    export default {render:function(variables){return ${parsed.compileJSVue()}}, ...scriptCode}
 `;
-    } else {
-        let code = parsed.compileJSVue();
-        return `
-    import { ref, h } from 'vue'
-export default function (variables){return ${code}}
-`;
-    }
 }
