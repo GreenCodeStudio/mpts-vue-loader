202f1d464f253da3468dbc3ac838bb30555254b7
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-12-04 00:16:22 +0100

    better working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const XMLParser = require("mpts-core/dist/parser/XMLParser").XMLParser;
-
+const TDocumentFragment = require("mpts-core/dist/nodes/TDocumentFragment.js").TDocumentFragment;
 
 module.exports = function loader(input) {
     let parsed = XMLParser.Parse(input)
-    let code = parsed.compileJSVue();
-    return `
+    const script = parsed.children.find(x => x?.tagName?.toLocaleLowerCase() == 'script');
+    if (script) {
+        let parsed2 = new TDocumentFragment()
+        parsed2.children = parsed.children.filter(x => x !== script);
+        let scriptContent = script.children.map(x => x.text).join('\n');
+        return `
+    import { ref, h } from 'vue'
+    import scriptCode from 'data:application/javascript;base64,${btoa(scriptContent)}'
+export default {render:function(){return ${parsed2.compileJSVue()}}, ...scriptCode}
+`;
+    } else {
+        let code = parsed.compileJSVue();
+        return `
     import { ref, h } from 'vue'
 export default function (variables){return ${code}}
 `;
+    }
 }
